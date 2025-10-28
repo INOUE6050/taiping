b998c22de575a9f385632962c9e9176ac0dcd761
shota <inoshi6050@gmail.com>
2025-10-28 19:07:07 +0900

    B. 結果入力部分は終了 時間と文章を入れれば、3分あたりの入力文字数が表示されるようになった。
    今後追加する必要がある仕様としては、入力した結果をcsvに入力する必要がある。（C.結果リスト表示のため）

---
+++ src/main/Menu.java
@@ -0,0 +1,44 @@
+package main;
+
+import java.util.Scanner;
+
+public class Menu {
+    public static void menu(){
+        Scanner scanner = new Scanner(System.in);
+        boolean isContinue = true;
+        while(isContinue){
+            System.out.println("""
+                    -----------Menu-----------
+                      A. Taiping URL
+                      B. Result input
+                      C. List of past results
+                      Q. Quit
+                    --------------------------
+                    """);
+            System.out.print("Please select a menu > ");
+            String userInput = scanner.nextLine();
+            switch (userInput){
+                case "A" -> {
+                    System.out.println("Selected A");
+                    Utils.openTaipingUPL();
+                }
+                case "B" -> {
+                    System.out.println("Selected B");
+                    Display.disOneResult(UserInput.inputResult());
+                }
+                case "C" -> {
+                    System.out.println("Selected C");
+
+                }
+                case "Q" -> {
+                    System.out.println("Selected Q");
+                    System.out.println("Exiting the app");
+                    isContinue = false;
+                }
+                default -> {
+                    System.out.println("Please enter either A, B, or C.");
+                }
+            }
+        }
+    }
+}
