b10d0ac9726827495326edfe25b098dccd6910db
shota <inoshi6050@gmail.com>
2025-10-30 13:51:59 +0900

    B. 結果入力部分は終了 時間と文章を入れれば、3分あたりの入力文字数が表示されるようになった。
    今後追加する必要がある仕様としては、入力した結果をcsvに入力する必要がある。（C.結果リスト表示のため）

--- src/main/Display.java
+++ src/main/Display.java
@@ -8,11 +8,14 @@ public class Display {
                          Time(seconds): %.0f
                        Character count: %.0f
                  Number of characters
+                        entered (/sec): %.2f
+                 Number of characters
                        entered (/3min): %.2f
                 -------------------------------
                 """,
                 d[0],
                 d[1],
+                Utils.chaperSec(d[1], d[0]),
                 Utils.threeMinAns(d[1], d[0])));
     }
 }
