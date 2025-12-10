e0fc017fa5ff45707e8e26b2728833f3d6a6e18f
shota <inoshi6050@gmail.com>
2025-12-10 17:18:54 +0900

    Graphicの修正

--- src/main/Main.java
+++ src/main/Main.java
@@ -5,9 +5,9 @@ import main.application.TypingApp;
 public class Main {
     public static void main(String[] args){
         System.out.println("""
-                |===========================|
+                |==========================|
                 |   Typing Level Checker   |
-                |===========================|
+                |==========================|
                 This app measures and stores 
                            your typing speed.""");
 
